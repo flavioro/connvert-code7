6d4c6d2780e11ec3115f6c2818a87a368d5464d0
flavioro <flavioro@gmail.com>
2021-04-25 22:36:13 -0300

    Create structure initial

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+import routes from './routers'
+dotenv.config()
+
+class App {
+
+}
+
+export default new App()
