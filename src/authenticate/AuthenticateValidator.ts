1c4ecb3497087a22bb52b9cbde02c448764b6a37
flavioro <flavioro@gmail.com>
2021-04-29 16:02:52 -0300

    Correction to routers/index

--- src/authenticate/AuthenticateValidator.ts
+++ src/authenticate/AuthenticateValidator.ts
@@ -9,11 +9,12 @@ function authValidator (request: Request, response: Response, next : NextFunctio
   const authHeader = (request.headers.authorization || '')
   const status = UNAUTHORIZED
   if (!authHeader) {
+    console.log('Token JWT não está presente.')
     response.status(status).send(new ResponseError('Token JWT não está presente.', status))
   }
-
+  
   const [bearer, token] = authHeader.split(' ')
-
+  
   try {
     if (bearer.trim().toLowerCase() !== 'bearer') {
       throw new ResponseError()
@@ -21,6 +22,7 @@ function authValidator (request: Request, response: Response, next : NextFunctio
     verify(token, auth.jwt.secret)
     next()
   } catch (error) {
+    console.log('Token JWT mal formado.')
     response.status(status).send(new ResponseError('Token JWT mal formado.', status))
   }
 }
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -10,19 +10,14 @@ import authValidator from '../authenticate/AuthenticateValidator'
 const routes = Router()
 routes.post('/v1/users/auth', AuthenticateUserController.Authenticate)
 routes.post('/v1/users/create', CreateUserController.CreateUser)
-routes.get('/v1/clients', apiDebts.getAll)
-
-function here() {
-  console.log('test ')
-}
 
 const authRoutes = Router()
-// authRoutes.get('/v1/clients', apiDebts.getAll)
-authRoutes.get('/v1/clients/:id', apiDebts.getById)
-authRoutes.get('/v1/clients/:id/debts', apiDebts.getDebtsByUser)
+routes.get('/v1/clients', authValidator, apiDebts.getAll)
+routes.get('/v1/clients/:id', authValidator, apiDebts.getById)
+routes.get('/v1/clients/:id/debts', authValidator, apiDebts.getDebtsByUser)
 
-authRoutes.post('/v1/debts', debtController.Create)
-authRoutes.put('/v1/debts/:id', debtController.Update)
-authRoutes.delete('/v1/debts/:id', debtController.Delete)
+routes.post('/v1/debts', authValidator, debtController.Create)
+routes.put('/v1/debts/:id', authValidator, debtController.Update)
+routes.delete('/v1/debts/:id', authValidator, debtController.Delete)
 
 export default routes
