b5035635dc265077a93a72213522b1db8320e3c1
flavioro <flavioro@gmail.com>
2021-04-29 13:31:50 -0300

    Update routes

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -10,18 +10,19 @@ import authValidator from '../authenticate/AuthenticateValidator'
 const routes = Router()
 routes.post('/v1/users/auth', AuthenticateUserController.Authenticate)
 routes.post('/v1/users/create', CreateUserController.CreateUser)
+routes.get('/v1/clients', apiDebts.getAll)
 
 function here() {
-  console.log('CreateUser ')
+  console.log('test ')
 }
 
 const authRoutes = Router()
-authRoutes.get('/v1/clients', authValidator, apiDebts.getAll)
-authRoutes.get('/v1/clients/:id', authValidator, apiDebts.getById)
-authRoutes.get('/v1/clients/:id/debts', authValidator, apiDebts.getDebtsByUser)
+// authRoutes.get('/v1/clients', apiDebts.getAll)
+authRoutes.get('/v1/clients/:id', apiDebts.getById)
+authRoutes.get('/v1/clients/:id/debts', apiDebts.getDebtsByUser)
 
-authRoutes.post('/debts', authValidator, debtController.Create)
-authRoutes.put('/debts/:id', authValidator, debtController.Update)
-authRoutes.delete('/debts/:id', authValidator, debtController.Delete)
+authRoutes.post('/v1/debts', debtController.Create)
+authRoutes.put('/v1/debts/:id', debtController.Update)
+authRoutes.delete('/v1/debts/:id', debtController.Delete)
 
 export default routes
